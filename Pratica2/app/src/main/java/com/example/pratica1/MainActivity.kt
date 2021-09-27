a0689642418b19eaaa86f4b6cb7831f4ea3ac839
Bielsaar <bielsaar@gmail.com>
2021-09-27 17:46:15 -0300

    Implementação - Pratica Semana 2

--- Pratica2/app/src/main/java/com/example/pratica1/MainActivity.kt
+++ Pratica2/app/src/main/java/com/example/pratica1/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.pratica1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var txtVwInfoSobre: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.txtVwInfoSobre = findViewById(R.id.txtVwInfoSobre);
+    }
+
+    fun MostrarInfo(view: View){
+        this.txtVwInfoSobre.text = "Resgata o numero de série do dispositivo"
     }
 }
